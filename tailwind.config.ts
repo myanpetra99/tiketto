f6a12f15f4370e661b6e184260140c5dd99cf015
myanpetra99 <myanpetra99@gmail.com>
2023-09-30 02:45:05 +0700

    add safelist classes tailwind color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,21 @@
 import { type Config } from "tailwindcss";
+const colors = [
+  'gray', 'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'
+];
+const shades = [
+  '100', '200', '300', '400', '500', '600', '700', '800', '900'
+];
+
+const bgClasses:string[] = [];
+
+colors.forEach(color => {
+  shades.forEach(shade => {
+    bgClasses.push(`bg-${color}-${shade}`);
+  });
+});
+
+
+
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
@@ -10,4 +27,12 @@ export default {
     },
   },
   plugins: [],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    options: {
+      safelist: bgClasses, // Your generated array
+    },
+  },
 } satisfies Config;
+
+
